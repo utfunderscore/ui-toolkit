54acad8aa76edd6b31b1f06cb5c6f5ad0c469224
utfunderscore <83981186+utfunderscore@users.noreply.github.com>
2025-07-20 21:16:45 +0100

    feat: Add temporary alignment guide for hotbar ui

--- src/main/java/org/readutf/ui/hud/HotbarHud.java
+++ src/main/java/org/readutf/ui/hud/HotbarHud.java
@@ -1,5 +1,6 @@
 package org.readutf.ui.hud;
 
+import net.kyori.adventure.text.Component;
 import org.readutf.ui.Constants;
 import org.readutf.ui.Module;
 import org.readutf.ui.utils.FontUtils;
@@ -18,13 +19,20 @@ public class HotbarHud implements Module {
         this.texture = Constants.createTexture(writable);
     }
 
+    public Component getText() {
+        return Component.text(String.valueOf(character));
+    }
+
     @Override
     public void apply(ResourcePack resourcePack) {
         BitMapFontProvider rightFont = FontProvider.bitMap()
                 .file(texture.key())
                 .height(25)
-                .ascent(25)
+                .ascent(-10)
                 .characters(String.valueOf(character))
                 .build();
+
+        resourcePack.texture(texture);
+        FontUtils.appendFont(resourcePack, rightFont);
     }
 }
